b73eb93732497cdaa00eb3038d88bdadf8f5f96e
Lam Nhan <nhan.lh@raksul.com>
2023-08-13 00:48:55 +0700

    Remove docs

--- docs/base.8288dbd5.js
+++
@@ -1,5 +0,0 @@
-var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire2169;e.register("e9F1h",function(n,s){Object.defineProperty(n.exports,"AppPageComponentsBase",{get:function(){return i},set:void 0,enumerable:!0,configurable:!0});var t=e("l73V3"),a=e("5B3HT"),o=e("aPFYP");let i=class extends a.TiniComponent{render(){return(0,a.html)`
-      <h1>Bases</h1>
-      <p>// TODO: write docs ...</p>
-    `}};i=(0,t.__decorate)([(0,a.Page)({name:"app-page-components-base",theming:{styling:(0,a.stylingWithBases)([o.commonBases,o.headingsBases,o.linkBases,o.textBases,o.codeBases])}})],i)});
-//# sourceMappingURL=base.8288dbd5.js.map
--- docs/get-started.7600ef81.js
+++
@@ -1,5 +0,0 @@
-var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire2169;e.register("eowYc",function(t,n){Object.defineProperty(t.exports,"AppPageGetStarted",{get:function(){return o},set:void 0,enumerable:!0,configurable:!0});var a=e("l73V3"),s=e("5B3HT"),i=e("aPFYP");let o=class extends s.TiniComponent{render(){return(0,s.html)`
-      <h1>Get started</h1>
-      <p>// TODO: write docs ...</p>
-    `}};o=(0,a.__decorate)([(0,s.Page)({name:"app-page-get-started",theming:{styling:(0,s.stylingWithBases)([i.commonBases,i.headingsBases,i.linkBases,i.textBases])}})],o)});
-//# sourceMappingURL=get-started.7600ef81.js.map
