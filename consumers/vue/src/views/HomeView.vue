20e2c4c1cb24553d9d1f2091b4a437f96a4ec10c
Lam Nhan <nhan.lh@raksul.com>
2023-09-24 17:09:41 +0700

    Init Vue

---
+++ consumers/vue/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {useComponents} from 'tinijs';
+import {TiniButtonComponent} from '@tinijs/ui-bootstrap/components/button.js';
+
+useComponents([TiniButtonComponent]);
+</script>
+
+<template>
+  <main>
+    <tini-button scheme="primary">A Button</tini-button>
+  </main>
+</template>
