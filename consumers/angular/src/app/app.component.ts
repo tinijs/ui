2c27fc254ccb6c66bb90683ca7a90fa34a50d8c8
Lam Nhan <nhan.lh@raksul.com>
2023-11-24 15:17:45 +0700

    Init Angular

---
+++ consumers/angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {useComponents} from 'tinijs';
+
+import {TiniButtonComponent} from '@tinijs/ui-bootstrap/components/button';
+import {IconHeartFillComponent} from '@tinijs/bootstrap-icons/heart-fill';
+
+useComponents([
+  TiniButtonComponent,
+  IconHeartFillComponent,
+]);
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {}
