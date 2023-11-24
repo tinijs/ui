856a5f8d284fe60cb28f03722c47cc43263969ba
Lam Nhan <nhan.lh@raksul.com>
2023-11-24 15:57:38 +0700

    Init Svelte

---
+++ consumers/svelte/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+import { useComponents } from 'tinijs';
+
+import {TiniButtonComponent} from '@tinijs/ui-bootstrap/components/button';
+import {IconHeartFillComponent} from '@tinijs/bootstrap-icons/heart-fill';
+
+useComponents([
+  TiniButtonComponent,
+  IconHeartFillComponent
+]);
+
+</script>
+
+<main>
+  <h1>Svelte</h1>
+  
+  <div class="buttons">
+    <tini-button scheme="primary">A Button</tini-button>
+    <tini-button scheme="success" hoverMap={{scheme: 'gradient-success'}}>A Button</tini-button>
+  </div>
+
+  <div class="icons">
+    <icon-heart-fill scheme="gradient-disco-club"></icon-heart-fill>
+  </div>
+
+</main>
